61aa241db0c442075b2f25adf49de58c7b3040a5
Elise Shaffer <elise@eliseshaffer.com>
2023-08-28 12:09:52 -0600

    Fix LSP

---
+++ lua/lsp.lua
@@ -0,0 +1,124 @@
+local nvim_lsp = require('lspconfig')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+--local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true
+}
+
+-- require('lspkind').init()
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = false,
+})
+
+-- Diagnostics symbols for display in the sign column.
+local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+vim.cmd('setlocal omnifunc=v:lua.vim.lsp.omnifunc')
+
+nvim_lsp.html.setup {
+  filetypes = { "html", "eruby" },
+  capabilities = capabilities
+}
+nvim_lsp.tsserver.setup {
+  capabilities = capabilities
+}
+nvim_lsp.solargraph.setup {
+  capabilities = capabilities
+}
+
+nvim_lsp.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+-- nvim_lsp.steep.setup{}
+
+nvim_lsp.cssls.setup {
+  capabilities = capabilities
+}
+nvim_lsp.dockerls.setup {
+  capabilities = capabilities
+}
+nvim_lsp.jsonls.setup {
+  commands = {
+    Format = {
+      function()
+        vim.lsp.buf.range_formatting({}, { 0, 0 }, { vim.fn.line("$"), 0 })
+      end
+    }
+  },
+  capabilities = capabilities
+}
+nvim_lsp.yamlls.setup {
+  capabilities = capabilities
+}
+nvim_lsp.vimls.setup {
+  capabilities = capabilities
+}
+nvim_lsp.eslint.setup {
+  on_attach = function(client, _)
+    -- nvim_lsp.services.commmon_on_attach(client, bufnr)
+    client.resolved_capabilities.document_formatting = true
+    --vim.cmd('autocmd BufWritePre <buffer> EslintFixAll')
+  end
+}
+
+-- Create a custom namespace. This will aggregate signs from all other
+-- namespaces and only show the one with the highest severity on a
+-- given line
+local ns = vim.api.nvim_create_namespace("my_namespace")
+
+-- Get a reference to the original signs handler
+local orig_signs_handler = vim.diagnostic.handlers.signs
+
+-- Override the built-in signs handler
+vim.diagnostic.handlers.signs = {
+  show = function(_, bufnr, _, opts)
+    -- Get all diagnostics from the whole buffer rather than just the
+    -- diagnostics passed to the handler
+    local diagnostics = vim.diagnostic.get(bufnr)
+
+    -- Find the "worst" diagnostic per line
+    local max_severity_per_line = {}
+    for _, d in pairs(diagnostics) do
+      local m = max_severity_per_line[d.lnum]
+      if not m or d.severity < m.severity then
+        max_severity_per_line[d.lnum] = d
+      end
+    end
+
+    -- Pass the filtered diagnostics (with our custom namespace) to
+    -- the original handler
+    local filtered_diagnostics = vim.tbl_values(max_severity_per_line)
+    orig_signs_handler.show(ns, bufnr, filtered_diagnostics, opts)
+  end,
+  hide = function(_, bufnr)
+    orig_signs_handler.hide(ns, bufnr)
+  end,
+}
