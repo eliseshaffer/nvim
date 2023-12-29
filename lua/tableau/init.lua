ab0b610406805b588ed8335c067ecb4300ef2a14
Elise Shaffer <elise@eliseshaffer.com>
2023-12-29 15:18:33 -0700

    Extract TabLabel into a file

--- lua/tableau/init.lua
+++ lua/tableau/init.lua
@@ -14,22 +14,7 @@ local M = {}
 local utils = require('tableau.utils')
 local Config = require('tableau.config').current()
 local Buffer = require('tableau.buffer')
-local TabLabel = {}
-
-function TabLabel:new(tab_id)
-  local o = {
-    hl = utils.get_highlight_group_for_tab(tab_id),
-    place = vim.api.nvim_tabpage_get_number(tab_id)
-  }
-  setmetatable(o, self)
-  self.__index = self
-  return o
-end
-
-function TabLabel:render()
-  -- return self.hl .. self.name
-  return self.hl .. "%" .. self.place .. "T" .. self.hl .. " " .. self.place .. ":"
-end
+local TabLabel = require('tableau.tablabel')
 
 -- tab = {
 --   hl = "TableauCurrentInactive",
