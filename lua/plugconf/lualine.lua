836700e4b1d402ded30687b64c359629c6a32258
Elise Shaffer <elise@eliseshaffer.com>
2024-01-02 14:07:52 -0700

    Add lsp infromation to statusline

--- lua/plugconf/lualine.lua
+++ lua/plugconf/lualine.lua
@@ -1,14 +1,29 @@
+local lsp_status = function()
+  local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+  if #clients == 0 then
+    return "LSP Inactive"
+  end
+
+  local client_names = {}
+  for _, client in pairs(clients) do
+    table.insert(client_names, client.name)
+  end
+
+  return table.concat(client_names, ",")
+end
+
 require('lualine').setup({
+
   options = {
     section_separators = '',
     component_separators = '',
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = {},
-    lualine_c = { 'branch', 'diff', 'diagnostics' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype', 'progress', 'location' },
-    lualine_y = {},
-    lualine_z = {}
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'filetype' },
+    lualine_y = { 'diagnostics', lsp_status },
+    lualine_z = { 'progress', 'location' }
   },
 })
