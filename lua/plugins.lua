9415fa7619960164fe347b3dc505b00b97887c6d
Elise Shaffer <elise@eliseshaffer.com>
2024-01-02 20:03:41 -0700

    Add eslint config to Neovim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -213,12 +213,15 @@ local plugins = {
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
         lsp.default_keymaps({ buffer = bufnr })
-        navic.attach(client, bufnr)
+        if client.server_capabilities.documentSymbolProvider then
+          navic.attach(client, bufnr)
+        end
       end)
 
       -- (Optional) Configure lua language server for neovim
       require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
       require('lspconfig').solargraph.setup({})
+      require('lspconfig').eslint.setup({})
 
       lsp.setup()
       -- Make sure you setup `cmp` after lsp-zero
