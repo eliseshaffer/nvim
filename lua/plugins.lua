d8e0f9ba36a71d0449cb651ae77ff78ea78359fd
Elise Shaffer <elise@eliseshaffer.com>
2024-04-15 15:56:07 -0600

    Remove Tableau config
    I am using the default config, so I don't need this in the regular
    config.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,43 +30,7 @@ local plugins = {
   {
     "eliseshaffer/tableau.nvim",
     config = function()
-      require('tableau').setup({
-        hl_groups = {
-          {
-            name = "TableauBackground",
-            fg = "#24273a",
-            bg = "#24273a"
-          },
-          {
-            name = "TableauCurrentInactive",
-            fg = "#24273a",
-            bg = "#d0b0ff"
-          },
-          {
-            name = "TableauCurrentActive",
-            fg = "#24273a",
-            bg = "#b690d6",
-          },
-          {
-            name = "TableauOtherInactive",
-            fg = "#d0b0ff",
-            bg = "#24273a",
-          },
-          {
-            name = "TableauOtherActive",
-            fg = "#b690d6",
-            bg = "#24273a",
-          },
-        },
-        hidden_buffer_types = {
-          "NeogitStatus",
-          "NeogitPopup",
-          "NeogitCommitMessage",
-          "nofile",
-          "prompt",
-          "TelescopePrompt",
-        }
-      })
+      require('tableau').setup({})
     end,
   },
   {
