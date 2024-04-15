5932cb2c719495c87af3627cf9030a36c0bcf041
Elise Shaffer <elise@eliseshaffer.com>
2024-04-15 17:22:49 -0600

    Move catppuccin config to plugin file

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,29 @@ return {
   "catppuccin/nvim",
   as = "catppuccin",
   config = function()
-    require 'plugconf.catppuccin'
+    require("catppuccin").setup(
+      {
+        background = {
+          -- :h background
+          light = "latte",
+          dark = "macchiato",
+        },
+        integrations = {
+          cmp = true,
+          alpha = true,
+          gitsigns = true,
+          neotree = true,
+          neogit = true,
+          telescope = true,
+          notify = false,
+          mini = false,
+          which_key = true,
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      }
+    )
+
+    vim.go.background = vim.env.COLOR
+    vim.cmd [[colorscheme catppuccin]]
   end
 }
