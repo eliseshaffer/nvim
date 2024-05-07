1262597474bd7b1044d0aea0dd3db0a48839b43f
Elise Shaffer <elise@eliseshaffer.com>
2024-05-07 11:48:56 -0400

    Add sessionable

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,1 +1,17 @@
-return {}
+return {
+  {
+    'blaineventurine/sessionable',
+    dependencies = { 'nvim-telescope/telescope.nvim' },
+    config = {
+      session_dir = vim.fn.stdpath("data") .. "/sessions/",
+    }
+  }
+  -- {
+  --   'akinsho/bufferline.nvim',
+  --   version = "*",
+  --   dependencies = 'nvim-tree/nvim-web-devicons',
+  --   config = function()
+  --     require("bufferline").setup {}
+  --   end,
+  -- }
+}
