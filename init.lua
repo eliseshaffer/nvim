5a76dce6a87e878f2e53e87d847afd6abbe703fe
Elise Shaffer <elise.shaffer@shopify.com>
2024-05-16 13:49:26 -0600

    Remove encoding stuff for 0.10

--- init.lua
+++ init.lua
@@ -28,11 +28,6 @@ vim.o.timeout = true
 vim.o.timeoutlen = 300
 vim.g.mapleader = ","
 vim.opt.mouse = "a"
-if vim.fn.has("termguicolors") == 1 then
-  vim.go.t_8f = "[[38;2;%lu;%lu;%lum"
-  vim.go.t_8b = "[[48;2;%lu;%lu;%lum"
-  vim.opt.termguicolors = true
-end
 
 -- -------------------------------------------------------------------------------------------
 --
