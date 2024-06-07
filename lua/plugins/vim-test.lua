dc197538d5d4d73a8b610eb6801f3a39a45a04d2
Elise Shaffer <elise.shaffer@shopify.com>
2024-06-07 11:17:37 -0600

    Change vim-test window location

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -2,6 +2,6 @@ return {
   'vim-test/vim-test',
   config = function()
     vim.g['test#strategy'] = "neovim"
-    vim.g['test#neovim#term_position'] = "vert bo 100"
+    vim.g['test#neovim#term_position'] = "bot"
   end
 }
